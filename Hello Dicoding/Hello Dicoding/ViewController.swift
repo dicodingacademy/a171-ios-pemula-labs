3c1b48efcffcbad42cddea9d062da1ff2203d524
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2020-01-28 11:44:05 +0700

    Add Hallo Dicoding App

---
+++ Hello Dicoding/Hello Dicoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello Dicoding
+//
+//  Created by Gilang Ramadhan on 28/01/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
