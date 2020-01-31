b9c7ebd479dcfa42c447463b06c86ba07842e6b2
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2020-01-31 14:50:59 +0700

    Resolve Bug Fixing

--- SimpleCalculator/SimpleCalculator/ViewController.swift
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var hight: UITextField!
+    @IBOutlet weak var height: UITextField!
     @IBOutlet weak var width: UITextField!
     @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculate(_ sender: Any) {
-        if let h = Double(hight.text ?? "0"), let w = Double(width.text ?? "0"){
+        if let h = Double(height.text ?? "0"), let w = Double(width.text ?? "0"){
             result.text = String(h * w)
         }else{
             result.text = "Hasil Tidak Valid"
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
     
     @IBAction func reset(_ sender: Any) {
         result.text = ""
-        hight.text = ""
+        height.text = ""
         width.text = ""
     }
 }
