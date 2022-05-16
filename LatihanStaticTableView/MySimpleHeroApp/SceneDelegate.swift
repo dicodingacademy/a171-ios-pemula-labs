5c0398ba00523cc38338e8b1deaddbdb3fddf933
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-05-17 00:30:43 +0700

    Add Dynamic Table View and Update Static Table View

--- LatihanStaticTableView/MySimpleHeroApp/SceneDelegate.swift
+++ LatihanStaticTableView/MySimpleHeroApp/SceneDelegate.swift
@@ -10,6 +10,6 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
 }
--- LatihanStaticTableView/MySimpleHeroApp/ViewController.swift
+++ LatihanStaticTableView/MySimpleHeroApp/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
 }
